8ede8365a164214986870808587ee829623b9453
Andrey Narut <andrey.narut.90@gmail.com>
2018-04-12 16:17:45 +0300

    Add AoC solutions.

---
+++ adventofcode/src/main/groovy/year_2015/day_1/DirectionsDecoder.groovy
@@ -0,0 +1,58 @@
+package year_2015.day_1
+
+class DirectionsDecoder {
+    
+    private String instructions
+
+    DirectionsDecoder(String instructions) {
+        this.instructions = instructions
+    }
+    
+    int getFloor() {
+        int result = 0
+
+        for (int i = 0; i < instructions.length(); i++) {
+            switch (instructions[i]) {
+                case '(':
+                    result++
+                    break
+                case ')':
+                    result--
+                    break
+                default:
+                    throw new Exception("incorrect instructions: ${instructions[i]}")
+            }
+        }
+        result
+    }
+
+    int getPosition() {
+        int position = 0
+        int result = -1
+        
+        for (int i = 0; i < instructions.length() && result == -1 ; i++) {
+            switch (instructions[i]) {
+                case '(':
+                    position++
+                    break
+                case ')':
+                    position--
+                    break
+                default:
+                    throw new Exception("incorrect instructions: ${instructions[i]}")
+            }
+            if (position == -1) {
+                result = i + 1
+            }
+        }
+        result
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        println new DirectionsDecoder("()()()()()(((()))()))))))))").position
+    }
+
+
+}
